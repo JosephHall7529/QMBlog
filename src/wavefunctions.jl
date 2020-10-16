19fde951f0643e7782c6cda2e342247f95e7f0be
Joe <jhall7529@gmail.com>
2020-10-16 17:04:09 +1030

    Development
    extended the definition of the Probabilty ampliutude function to include 
    the new localised wavefunction

--- src/wavefunctions.jl
+++ src/wavefunctions.jl
@@ -65,25 +65,30 @@ the probability amplitude is given as the modulus squared of the wavefunction
 
 # input
 x::Float64
-::Array{Float64,1} - the spacial values
+ ::Array{Float64,1} - the spacial values
 t::Float64
-::Array{Float64,1} - the time values
+ ::Array{Float64,1} - the time values
 p::Float64
-::Array{Float64,1} - the momentum values
+ ::Array{Float64,1} - the momentum values
 m::Float64 - the mass of the particle
 free::String - are we dealing with a free particle or not
+localized::String - are we dealing with a localized particle or not
 
 # output
 P::Array{Complex,3} - x is horizontal
-                  p is vertical
-                  t is depth
+                      p is vertical
+                      t is depth
 
 # moral
 the probability is 1 everywhere with the old wavefunction defn
 """
-function P(x::Float64,t::Float64; p::Float64=x, m::Float64=1., free::String="y")
-    return real(ψ(x,t; p=p, m=m, free=free).*conj(ψ(x,t; p=p, m=m, free=free)))
+function P(x::Float64,t::Float64; p::Float64=x, m::Float64=1., free::String="y", localized::String="n",
+        len::Int=1000)
+    return real(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len).*
+        conj(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len)))
 end
-function P(x::Array{Float64,1},t::Array{Float64,1}; p::Array{Float64,1}=x, m::Float64=1., free::String="y")
-    return real(ψ(x,t; p=p, m=m, free=free).*conj(ψ(x,t; p=p, m=m, free=free)))
+function P(x::Array{Float64,1},t::Array{Float64,1}; p::Array{Float64,1}=x, m::Float64=1., free::String="y",
+            localized::String="n", len::Int=1000)
+    return real(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len).*
+        conj(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len)))
 end
