4e402680ecc7740a201797a59b65172f204415f4
Joe <jhall7529@gmail.com>
2020-10-20 10:43:16 +1030

    bug fix, development
    The range of the momentum was written in a way that gave incorrect 
    wavefunctions.
    
    I have included a pseudorandom generator in order to allow 
    reproducibility

--- src/wavefunctions.jl
+++ src/wavefunctions.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra
+using LinearAlgebra, Random
 
 include("../../Constants/src/Short.jl")
 
@@ -8,54 +8,79 @@ this example only takes into account one space dimension
 
 # input
 x::Float64
+
  ::Array{Float64,1} - the spacial values
 t::Float64
+
  ::Array{Float64,1} - the time values
+
 p::Float64
+
  ::Array{Float64,1} - the momentum values
+
 m::Float64 - the mass of the particle
+
 free::String - are we dealing with a free particle or not
+
 localized::String - are we dealing with a localized particle or not
+
 len::Int - the number of values we give to the integral over p
 
 # output
 ψ::Array{Complex,3} - x is horizontal
+
                       p is vertical
+
                       t is depth
+
+ψ::Array{Complex,3} - x is vertical
+
+                    t is horizontal
 
 # moral
 we remove the dependence on p if the particle is localized, no longer requiring it as a variable
+
 now there is also no mechanism to get ϕ(p)
 """
-function ψ(x::Float64,t::Float64; p::Float64=x, m::Float64=1., free::String="y", localized::String="n",
-            len::Int=1000)
+function ψ(x::Float64,t::Float64=1.; p::Float64=x, m::Float64=1., free::String="y", localized::String="y",
+            len::Int=1000, L::Float64=0.)
+    rng = Random.seed!(42)
     if free == "y"
         E = sqrt(dot(p.*c,p.*c) + (m*c^2)^2)
         if localized == "n"
             ψ = [exp(im*(dot(p[i],x[j]) - E*t) / ħ) for i in 1:length(p), j in 1:length(x), t in t]
             return ψ
         elseif localized == "y"
-            p = range(-abs(100*x), 100*x, length=len)
-            ϕ = rand(len)
+            if L == 0.
+                p = range(-abs(100*x), 100*x, length=len)
+            else
+                p = range(0, step=2π/L, length=len)
+            end
+            ϕ = rand(rng,len)
             N = 1/(2π*ħ)
             ψ = [exp(im*(dot(p[i],x[j]) - E*t) / ħ) for i in 1:len, j in 1:length(x), t in t]
-            return (1/sqrt(2π*ħ))*(1/len)*sum(ϕ[:] .* ψ for i in 1:len, j in 1:length(x), t in t, dims=1)
+            return reshape((1/sqrt(2π*ħ))*(1/len)*sum(ϕ.*ψ, dims=1),:,length(t))
         end
     end
 end
-function ψ(x::Array{Float64,1},t::Array{Float64,1}; p::Array{Float64,1}=x, m::Float64=1., free::String="y",
-            localized::String="n", len::Int=1000)
+function ψ(x::Array{Float64,1},t::Array{Float64,1}=[1.]; p::Array{Float64,1}=x, m::Float64=1., free::String="y",
+            localized::String="y", len::Int=1000, L::Float64=0.)
+    rng = Random.seed!(42)
     if free == "y"
         E = sqrt(dot(p.*c,p.*c) + (m*c^2)^2)
         if localized == "n"
             ψ = [exp.(im*(dot(p[i],x[j]) .- E*t) / ħ) for i in 1:length(p), j in 1:length(x), t in t]
             return ψ
         elseif localized == "y"
-            p = range(-abs(100*minimum(x)), 100*maximum(x), length=len)
-            ϕ = rand(len)
+            if L == 0.
+                p = collect(range(-abs.(100*maximum(x)), 100*maximum(x), length=len))
+            else
+                p = collect(range(0, step=2π/L, length=len))
+            end
+            ϕ = rand(rng,len)
             N = 1/(2π*ħ)
-            ψ = [exp(im*(dot(p[i],x[j]) - E*t) / ħ) for i in 1:len, j in 1:length(x), t in t]
-            return (1/sqrt(2π*ħ))*(1/len)*sum(ϕ[:] .* ψ, dims=1)
+            ψ = collect(exp.(im*(dot.(p[i],x[j]) .- E*t) / ħ) for i in 1:len, j in 1:length(x), t in t[:])
+            return reshape((1/sqrt(2π*ħ))*(1/len)*sum(ϕ.*ψ, dims=1),:,length(t))
         end
     end
 end
@@ -82,7 +107,7 @@ P::Array{Complex,3} - x is horizontal
 # moral
 the probability is 1 everywhere with the old wavefunction defn
 """
-function P(x::Float64,t::Float64=[1.]; p::Float64=x, m::Float64=1., free::String="y", localized::String="y",
+function P(x::Float64,t::Float64=1.; p::Float64=x, m::Float64=1., free::String="y", localized::String="y",
         len::Int=1000, fn::String="ψ")
     if fn == "ψ"
         return real(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len).*
@@ -97,9 +122,9 @@ function P(x::Array{Float64,1},t::Array{Float64,1}=[1.]; p::Array{Float64,1}=x, 
         return real(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len).*
             conj(ψ(x,t; p=p, m=m, free=free, localized=localized, len=len)))
     elseif fn == "ϕ"
-        return real(ϕ(x; m=m, len=len)[:].*conj(ϕ(x; m=m, len=len)[:]))
+        return real(ϕ(x; m=m, len=len).*conj(ϕ(x; m=m, len=len)))
     end
-end 
+end
 
 @doc raw"""
     the momentum space wavefunction, or more exactly the Fourier transform of the position wavefunction
@@ -125,3 +150,5 @@ function ϕ(p::Array{Float64,1}; m::Float64=1., len::Int=1000)
     return (1/sqrt(2π*ħ))*(1/len)*sum(ψ .* [exp(-im*(dot(p[i],x[j])) / ħ)
             for i in 1:length(p), j in 1:len], dims=2)
 end
+
+export ψ
